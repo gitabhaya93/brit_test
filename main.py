d2f87de03784de4bd8f01af5998c047d445a305e
gitabhaya93 <142480889+gitabhaya93@users.noreply.github.com>
2023-09-01 14:17:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 from db import Base,engine
 from item import router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router, prefix="/items")
-
-# from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
     "https://thankful-river-01992e00f.3.azurestaticapps.net/"
