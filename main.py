0ea716f9b2dbea16f77f83ecee5c798eb3098a04
gitabhaya93 <142480889+gitabhaya93@users.noreply.github.com>
2023-09-01 14:17:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ app.include_router(router, prefix="/items")
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
-    "http://localhost",
-    "http://127.0.0.1:8000"
-    "http://localhost:8000",
-    "http://127.0.0.1:5500",
     "https://thankful-river-01992e00f.3.azurestaticapps.net/"
 ]
 app.add_middleware(
